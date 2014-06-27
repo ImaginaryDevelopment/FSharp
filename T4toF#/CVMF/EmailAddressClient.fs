d683e22a4274f972d00b736d9a83fce45090bb2f
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-06-27 17:26:40 -0400

    some t4 from C# into F# work

--- T4toF#/CVMF/EmailAddressClient.fs
+++ T4toF#/CVMF/EmailAddressClient.fs
@@ -8,5 +8,7 @@ open EmailAddress
 let address1 = EmailAddress.create "x@example.com"
 let address2 = EmailAddress.create "example.com"
 
+
+
 // code that uses the internals of the type fails to compile
 // let address3 = T.EmailAddress "bad email"
