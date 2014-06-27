b4994f2523dcea7d0fefb60be8a71e80675aa75b
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-06-27 15:58:45 -0400

    FSharp Via template

---
+++ FSharpWeb/FSharpWeb/Controllers/CarsController.fs
@@ -0,0 +1,16 @@
+namespace FSharpWeb.Controllers
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Net.Http
+open System.Web.Http
+open FSharpWeb.Models
+
+/// Retrieves values.
+type CarsController() =
+    inherit ApiController()
+
+    let values = [| { Make = "Ford"; Model = "Mustang" }; { Make = "Nissan"; Model = "Titan" } |]
+
+    /// Gets all values.
+    member x.Get() = values
