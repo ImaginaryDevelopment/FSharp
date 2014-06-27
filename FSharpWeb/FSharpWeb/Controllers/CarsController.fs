14445df19bcb048e828cabb26d1add24d4995109
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-06-27 17:46:22 -0400

    basic rendering works, next we need to make sure razor works

--- FSharpWeb/FSharpWeb/Controllers/CarsController.fs
+++ FSharpWeb/FSharpWeb/Controllers/CarsController.fs
@@ -2,7 +2,9 @@ namespace FSharpWeb.Controllers
 open System
 open System.Collections.Generic
 open System.Linq
+open System.Net
 open System.Net.Http
+open System.Net.Http.Headers
 open System.Web.Http
 open FSharpWeb.Models
 
@@ -11,9 +13,17 @@ type CarsController() =
     inherit ApiController()
 
     let values = [| { Make = "Ford"; Model = "Mustang" }; { Make = "Nissan"; Model = "Titan" } |]
+    [<Route("cars/html")>]
+    [<HttpGet>]
     member x.Html() =
-        let viewPath = System.Web.HttpContext.Current.Server.MapPath("""~/Views/CarsController/Cars.cshtml""")
+        
+        let viewPath = System.Web.HttpContext.Current.Server.MapPath("""~/Views/Cars.cshtml""")
+
         let template = System.IO.File.ReadAllText(viewPath)
-        ""
+        let rendered = RazorEngine.Razor.Parse(template) // https://github.com/Antaris/RazorEngine
+        let response = new HttpResponseMessage(HttpStatusCode.OK)
+        response.Content <- new StringContent(rendered)
+        response.Content.Headers.ContentType <- new MediaTypeHeaderValue("text/html")
+        response
     /// Gets all values.
     member x.Get() = values
