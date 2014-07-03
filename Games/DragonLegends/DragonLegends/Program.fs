ba3ebe9a79dee9b76e07893d705accf8096b2655
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-07-02 20:24:43 -0400

    starting a simple generator

---
+++ Games/DragonLegends/DragonLegends/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System
+
+[<EntryPoint>]
+let main argv = 
+    
+    printfn "%A" argv
+    let x = 30
+    let y = 25
+    let objects = 2
+    let seed = 1
+    WinForm.RunWinForm()
+    let quit = Console.ReadLine()
+    0 // return an integer exit code
---
+++ Games/DragonLegends/DragonLegends/WinForm.fs
@@ -0,0 +1,14 @@
+﻿module WinForm
+open System.Drawing
+open System.Windows.Forms
+
+let RunWinForm() =
+    use form =
+            let temp = new Form(MinimumSize = new Size(80,80))
+            temp.Resize.Add(fun _ -> temp.Invalidate())
+            let brush = new SolidBrush(Color.SkyBlue)
+            temp.Paint.Add( fun e-> 
+                let width,height = temp.Width-54, temp.Height-54
+                e.Graphics.FillPie(brush,32,32,width,height,0,290))
+            temp
+    Application.Run(form)
