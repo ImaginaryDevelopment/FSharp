d21ddc1ee6655905fbd7c46393a11a346d971115
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-07-03 22:48:04 -0400

    adding in more tests

--- Games/DragonLegends/DragonLegends.Test/ShapesTests.fs
+++ Games/DragonLegends/DragonLegends.Test/ShapesTests.fs
@@ -4,11 +4,18 @@ open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 open Swensen.Unquote
-
+open TestExtensions
 open Shapes
+
 [<TestClass>]
 type ShapesTests() = 
+   
     [<TestMethod>]
-    member x.``Assert Rectangle has measures``() =
-        let result = 2 + 1
-        result =? 4
+    member t.``Assert Rectangle has measures``() =
+        test <@ Rectangle 1<X> 2<Y> |> Option.isSome @>
+    [<TestMethod>]
+    member t.``Assert Rectangle rejects 0<X>``() =
+        test <@ Rectangle 0<X> 1<_> |> Option.isNone @>
+    [<TestMethod>]
+    member t.``Assert Rectangle rejects 0<Y>``() =
+        test <@ Rectangle 1<X> 0<Y> |> Option.isNone @>
