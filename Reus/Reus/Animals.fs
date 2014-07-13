2044a3000eb8754b211e5834fa0bbbe1d90488cd
ImaginaryDevelopment <ImaginaryDevelopment@users.noreply.github.com>
2014-07-13 16:15:18 -0700

    Update Animals.fs
    adding lots of animals

--- Reus/Reus/Animals.fs
+++ Reus/Reus/Animals.fs
@@ -3,4 +3,19 @@
         |Rabbit
         |Goat
         |Beaver
-
+        |Chicken
+        //| swamp giant + forest + animals =?
+        |Stoat
+        |Fox
+        |Frog
+        |PoisonDartFrog
+        |Iguana
+        |KangarooRat
+        |DesertTortoise
+        |Clownfish
+        |Rattlesnake
+        |Mackerel
+        |Parrotfish
+        |Seabass
+        |Marten
+        |Monal
