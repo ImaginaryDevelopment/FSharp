9934e35fcfb8636433a14538607f690d60e06798
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-07-14 17:00:27 -0400

    sql ado and linq to sql type provider consumption examples/attempts

---
+++ HelloCsvProvider/HelloCsvProvider/Cvs.fs
@@ -0,0 +1,14 @@
+﻿module HelloCsvProvider
+open FSharp.Data
+
+open Microsoft.FSharp.Data.TypeProviders
+
+
+let cvs =  Cvs.GetDataContext()
+cvs.DataContext.Log <- System.Console.Out
+let query = query {
+        for row in cvs.Member_Member do
+        join row2 in cvs.Admin_Org on (row.Org_id = row2.Org_id)
+        select (row.City, row.First_name,row2.Add_by)
+    }
+
---
+++ HelloCsvProvider/HelloCsvProvider/SqlAdo.fs
@@ -0,0 +1,22 @@
+﻿module HelloCsvProvider.SqlAdo
+//[<Literal>]
+//let connectionString = @"Data Source=(LocalDb)\v11.0;Initial Catalog=AdventureWorks2012;Integrated Security=True"
+//[<Literal>]
+//let query = "
+//    SELECT TOP(@TopN) FirstName, LastName, SalesYTD 
+//    FROM Sales.vSalesPerson
+//    WHERE CountryRegionName = @regionName AND SalesYTD > @salesMoreThan 
+//    ORDER BY SalesYTD
+//" 
+//
+//type SalesPersonQuery = SqlCommandProvider<query, connectionString>
+//let cmd = new SalesPersonQuery()
+//
+//cmd.AsyncExecute(TopN = 3L, regionName = "United States", salesMoreThan = 1000000M) 
+//|> Async.RunSynchronously
+
+//output
+//seq
+//    [("Pamela", "Ansman-Wolfe", 1352577.1325M);
+//     ("David", "Campbell", 1573012.9383M);
+//     ("Tete", "Mensa-Annan", 1576562.1966M)]
