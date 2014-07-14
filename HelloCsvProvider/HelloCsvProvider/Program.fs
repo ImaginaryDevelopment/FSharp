5800c20ba5caa0eb69fefbb679487240acff1b5d
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-07-14 16:55:59 -0400

    working on cleanup primarily

--- HelloCsvProvider/HelloCsvProvider/Program.fs
+++ HelloCsvProvider/HelloCsvProvider/Program.fs
@@ -1,19 +1,51 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+open HelloTypeProviders.HelloJsonProvider
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    printfn "%s is %i" HelloTypeProviders.HelloJsonProvider.simple.Name HelloTypeProviders.HelloJsonProvider.simple.Age
+
+    // simple json
+    printfn "%s is %i" simple.Name simple.Age
+    
+    // stocks
     let symbol = "MSFT"
     let nflx = HelloTypeProviders.HelloCsvProvider.Stocks.StockData symbol
     let current = nflx.Rows |> Seq.head
+    
     printfn "%s is %A as of %A" symbol current.Close current.Date
-
+    printfn "%A" nflx.Rows
+    // realms
     let realms = HelloTypeProviders.WarcraftJson.realmStatus ()
     let mapped = realms.Realms |> Seq.map (fun r -> r.Name) |> Seq.toList
-    let maslowRexxar = HelloTypeProviders.WarcraftJson.characterProvider "Rexxar" "Maslow"
     printfn "realms: %A" mapped
-    printfn "characterInfo for %s in %s %A" maslowRexxar.Name maslowRexxar.Realm maslowRexxar
+    for i in HelloCsvProvider.query do
+        printfn "%A" i
+
+    // characters
+
+    let maslow = HelloTypeProviders.WarcraftJson.characterProvider "Rexxar" "Maslow"
+    printfn "characterInfo for %s in %s %A" maslow.Name maslow.Realm maslow
+
+    // pets
+    let maslowPets = HelloTypeProviders.WarcraftJson.petProvider "Rexxar" "Maslow"
+    
+    printfn "pets for %s on %s" maslowPets.Name maslowPets.Realm
+    // printfn "pets for %s on %s pets: %A" maslowPets.Name maslowPets.Realm maslowPets.Pets
+    //[ for p in maslowPets.Pets.Collected -> p]
+    let maxNameLength = maslowPets.Pets.Collected
+                    |> List.map (fun x -> x.Name.Length)
+                    |> List.max
+        //|> List.maxBy (fun x->x.Name.Length)
+        //|> List.head
+        
+
+    for p in maslowPets.Pets.Collected do // http://langref.org/fsharp/numbers
+        printfn "%-10s %-10s" p.Name p.CreatureName
+
+    // keep console open
     System.Console.ReadLine()
     0 // return an integer exit code
--- HelloCsvProvider/HelloCsvProvider/WarcraftJson.fs
+++ HelloCsvProvider/HelloCsvProvider/WarcraftJson.fs
@@ -1,6 +1,6 @@
 ﻿module HelloTypeProviders.WarcraftJson
 open FSharp.Data
-
+// http://jsfiddle.net/Maslow/j2qfN/
 // sample urls so the type providers can look at sample data to generate types/intellisense
 let [<Literal>] wowRealmUri = "http://us.battle.net/api/wow/realm/status?json=realmstatus"
 let [<Literal>] characterUri = "http://us.battle.net/api/wow/character/Rexxar/Maslow"
@@ -8,7 +8,14 @@ let [<Literal>] characterPetsUri = "http://us.battle.net/api/wow/character/Rexxa
 
 type WowRealmStatusProvider = JsonProvider<wowRealmUri>
 type WowCharacterProvider = JsonProvider<characterUri>
+type WowPetProvider = JsonProvider<characterPetsUri>
+
+let realmStatus () = WowRealmStatusProvider.Load(wowRealmUri)
+
 let characterProvider realm characterName = 
     let uri = sprintf "http://us.battle.net/api/wow/character/%s/%s" realm characterName
     WowCharacterProvider.Load  uri
-let realmStatus () = WowRealmStatusProvider.Load(wowRealmUri)
+
+let petProvider realm characterName =
+    let uri = sprintf "http://us.battle.net/api/wow/character/%s/%s?fields=pets" realm characterName
+    WowPetProvider.Load  uri
