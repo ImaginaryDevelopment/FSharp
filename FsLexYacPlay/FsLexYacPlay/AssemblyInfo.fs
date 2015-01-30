9d9b2c92453b79e5f3353cb506ea4b0908f55561
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2015-01-30 16:57:49 -0500

    lexing and parsing is working!

---
+++ FsLexYacPlay/FsLexYacPlay/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module FsLexYacPlay.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FsLexYacPlay")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Dbee")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
---
+++ FsLexYacPlay/FsLexYacPlay/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+let x = "
+    SELECT x, y, z
+    FROM t1
+    LEFT JOIN t2
+    INNER JOIN t3 ON t3.ID = t2.ID
+    WHERE x = 50 AND y = 20
+    ORDER BY x ASC, y DESC, z
+"
+
+open System
+open Sql
+open Microsoft.FSharp.Text
+
+let ParseSql sql =
+    let lexbuf = Lexing.LexBuffer<_>.FromString sql
+    let output = SqlParser.start SqlLexer.tokenize lexbuf
+    printfn "parsed as: %A" output
+    output
+
+[<EntryPoint>]
+let main argv =
+    printfn "args:%A" argv
+    let parsed = ParseSql x
+
+    Console.WriteLine("(press any key)")
+    Console.ReadKey(true) |> ignore
+    0 // return an integer exit code
+
